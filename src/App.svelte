1b1b49e22d029956b9ebbe1d22ae21008e01d677
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-08 14:05:57 +0530

    first commit pintu securty guard

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<main>
+	<h1>Security Guard</h1>
+	
+	<div class="security_guard">
+		<div class="name">Pintu</div>
+		<div class="state">Odissa</div>
+		<div class="education">9th class</div>
+		<div class="working_at">Gopalan Mall, near BTM layout, Bangalore</div>
+	</div>
+</main>
+
+<style>
+</style>
