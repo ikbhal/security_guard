0dd2d41642608f6c91def66ce7e523be104bf56f
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-08 14:26:42 +0530

    adding tailwind working

--- src/App.svelte
+++ src/App.svelte
@@ -21,16 +21,28 @@ class SecurityGuard {
 		this.workingAt = workingAt;
 	}
 }
+
+var sg = new SecurityGuard("Pintu", "Odissa", "9th class", "Gopalan Mall, Bangalore");
+
 </script>
 
 <main>
-	<h1>Security Guard</h1>
+	<!-- tail wind css cdn -->
+	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+
+
+	<!-- trying https://blog.logrocket.com/how-to-use-tailwind-css-with-svelte/ -->
+
+	<h1 class="py-8 px-4 border border-indigo-900 shadow-lg">Hello {name}!</h1>
+  	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	
+	<h1>Pintu Security Guard</h1>
 	
 	<div class="security_guard">
-		<div class="name">Pintu</div>
-		<div class="state">Odissa</div>
-		<div class="education">9th class</div>
-		<div class="working_at">Gopalan Mall, near BTM layout, Bangalore</div>
+		<div class="name">{sg.name}</div>
+		<div class="state">{sg.state}</div>
+		<div class="education">{sg.education}</div>
+		<div class="working_at">{sg.workingAt}</div>
 	</div>
 </main>
 
